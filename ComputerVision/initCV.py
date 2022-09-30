ea9d5d6601f41fb177fb9c652bea4aff13397d9e
perihassanzadeh <70037334+perihassanzadeh@users.noreply.github.com>
2022-09-29 23:46:25 -0400

    Added new images and testing features

--- ComputerVision/initCV.py
+++ ComputerVision/initCV.py
@@ -2,14 +2,16 @@ import cv2
 import numpy as np
 
 #Read in image and convert it to gray, add blur
-image = cv2.imread("opencv_frame3.png")
+image = cv2.imread("opencv_frame4.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+blurredmore = cv2.GaussianBlur(gray, (9, 9), 0)
 canny = cv2.Canny(blurred, 20, 40)
+canny2 = cv2.Canny(blurredmore, 20, 40)
 
 #Dilate image to open up
 kernel = np.ones((3,3), np.uint8)
-dilated = cv2.dilate(canny, kernel, iterations=2)
+dilated = cv2.dilate(canny2, kernel, iterations=1)
 
 #Draw contours on image based on continuous average colors
 (contours, hierarchy) = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -17,4 +19,5 @@ cv2.drawContours(image, contours, -1, (0,255,0), 3)
 
 #Show image to user
 cv2.imshow('canny', image)
+
 cv2.waitKey()
