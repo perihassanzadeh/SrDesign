62fe6ef67aea13d288ab3ddd5913ee8fc025b7c6
Peri Hassanzadeh <plh25@pitt.edu>
2022-10-11 21:37:57 -0400

    Color Detection for First Image Completed

--- ComputerVision/initCV.py
+++ ComputerVision/initCV.py
@@ -50,9 +50,10 @@ def computeContours(frame):
     dilated = cv2.dilate(canny, kernel, iterations=3)
 
     (contours, hierarchy) = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+    outputArr = []
     # Index used to remove nested squares.
     index = 0
+    #print(len(contours))
     for cnt in contours:
         if (hierarchy[0,index,3] != -1):
             epsilon = (15/100)*cv2.arcLength(cnt, True)
@@ -62,26 +63,68 @@ def computeContours(frame):
 
             if (len(approx) == 4 and area > 264):
                 # Square detected. Draw onto original image.
+                x,y,w,h = cv2.boundingRect(cnt)
+                #print(x, y)
+                avgColor = np.array(cv2.mean(frame[y:y+h, x:x+h])).astype(int)
+                #print(avgColor)
                 cv2.drawContours(frame, [approx], 0, (0, 255, 0), 3)
+
+                blue = avgColor[0]/255
+                green = avgColor[1]/255
+                red = avgColor[2]/255
+                cmax =max(red, blue, green)
+                cmin = min(red, blue, green)
+                diff = cmax - cmin
+                hue =-1
+                saturation = -1
+
+                if(cmax==cmin):
+                	hue=0
+                elif(cmax==red):
+                	hue = (60*((green-blue)/diff)+360)%360
+                elif(cmax==green):
+                	hue = (60*((blue-red)/diff)+120)%360
+                elif(cmax == blue):
+                	hue = (60*((red-green)/diff)+240)%360
+
+
+                if(cmax==0):
+                	saturation=0
+                else:
+                	saturation=(diff/cmax)*100
+
+                value = cmax*100
+
+                avgColor[0], avgColor[1], avgColor[2] = hue, saturation, value
+                face = []
+                print(hue, saturation, value)
+                if 190 <= avgColor[0] <= 250 and 55 <= avgColor[1] <= 95 and 35 <= avgColor[2] <= 85:
+                	print('blue detected at', x, y)
+                	outputArr.append('blue')
+                elif (345 <= avgColor[0] or avgColor[0] <= 11) and 50 <= avgColor[1] <= 90 and 45 <= avgColor[2] <= 90:
+                	print('red detected at', x, y)
+                	outputArr.append('red')
+                elif avgColor[0] <= 36 and 65 <= avgColor[1] <= 99 and 60 <= avgColor[2] <= 99:
+                	print('orange detected at', x, y)
+                	outputArr.append('orange')
+                elif 100<= avgColor[0] <= 150 and 65 <= avgColor[1] <= 99 and 60 <= avgColor[2] <= 75:
+                	print('green detected at ', x, y)
+                	outputArr.append('green')
+                elif 40<= avgColor[0] <= 80 and 60 <= avgColor[1] <= 90 and 70 <= avgColor[2] <= 100:
+                	print('yellow detected at ', x, y)
+                	outputArr.append('yellow')
+                elif avgColor[1] <= 20 and 90 <= avgColor[2]:
+                	print('white detected at ', x, y)
+                	outputArr.append('white')
+
         index += 1
 
-    
-    #colorDetect(contours, epsilon, approx, area)
-
+    print(outputArr)
+    rev = outputArr[::-1]
+    print(rev)
+    #red, orange, green, orange, yellow, green, orange, white, blue
+    #Online Color Picker w/ HSV vals: https://colorpicker.me/#a0d0bb
     return frame2;
-
-
-# def colorDetect(contours, epsilon, approx, area):
-# 	cnt = contours	
-# 	print(cnt[0])
-
-# 	for c in contours:
-# 		A1=cv2.contourArea(c)
-# 		contour_id = contour_id+1
-
-# 		if A1 < 3000 and A1 > 1000:
-# 			perimeter = cv2.arcLength(c, True)
-
 
 
 if __name__ == '__main__':
